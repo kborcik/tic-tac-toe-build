45774a335a5bcd940ec4114f2cd63f0601752cb6
Kyle Borcik <kyleborcik@gmail.com>
2023-06-19 10:52:31 -0400

    build package for netlify

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Square from "./Square";
+import { useState } from "react";
 
 function App() {
+  const handleClick = () => {
+    setSquares(["", "", "", "", "", "", "", "", ""]);
+    setPlayer(true);
+  };
+
+  const calculateWinner = (array) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [6, 4, 2],
+    ];
+
+    for(let i=0; i<lines.length; i ++){
+      const [a,b,c] = lines[i]
+
+      if(
+        array[a] &&
+        array[a] === array[b] &&
+        array[a] === array[c]
+      ){
+        return  `${array[a]} won!`
+      }
+    }
+    return "Who will win?"
+  };
+  const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
+
+  const [player, setPlayer] = useState(true);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}>Reset</button>
+     
+      <div className="container">
+        {squares.map((value, index) => {
+          return (
+            <Square
+              squareValue={value}
+              index={index}
+              squares={squares}
+              setSquares={setSquares}
+              player={player}
+              setPlayer={setPlayer}
+            />
+          );
+        })}
+      </div>
+      <span>{calculateWinner(squares)}</span>
     </div>
   );
 }
